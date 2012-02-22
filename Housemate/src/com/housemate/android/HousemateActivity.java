60c0183e38334569e041c713a3d15fd9d145a336
Michael Polycarpou <michaelpolycarpou@gmail.com>
2012-02-22 18:34:34 +0000

    test

--- Housemate/src/com/housemate/android/HousemateActivity.java
+++ Housemate/src/com/housemate/android/HousemateActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class HousemateActivity extends Activity {
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
